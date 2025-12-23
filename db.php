25f75b039401aa9e587d06db6def0f258f93919e
Semzhanski1 <99734761+Semzhanski1@users.noreply.github.com>
2025-12-24 00:09:52 +0300

    Add files via upload
    там вот в дб файле нужно думаю будет чет сделать или добавить или отбавить вообщем посмотри пж

--- db.php
+++ db.php
@@ -1,20 +1,13 @@
 <?php
 $host = 'localhost';
-$db   = 'studlib';
-$user = 'root'; // если XAMPP/macOS по умолчанию
-$pass = '';     // пароль по умолчанию пустой
-$charset = 'utf8mb4';
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
+$db   = 'studlib';  
+$user = 'root';
+$pass = '';
 
 try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e) {
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Ошибка БД: " . $e->getMessage());
 }
+?>
